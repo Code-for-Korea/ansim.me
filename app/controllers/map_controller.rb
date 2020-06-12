8fdb760146de9cb3908794d704cd5157e7ef2d39
rest515 <rest515@gmail.com>
2020-06-13 00:12:10 +0900

    카테고리별로 지도를 탐색할 수 있습니다

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,16 +2,38 @@ class MapController < ApplicationController
   def index
     @places = []
 
-    @shelters = Shelter.limit(50)
-    @shelters.each do |shelter|
-      @places << [
-        shelter.id,
-        shelter.status,
-        shelter.name,
-        shelter.road_address,
-        shelter.y.to_f,
-        shelter.x.to_f
-      ]
+    @category = params[:category] || "maskstores"
+
+    if @category == "maskstores"
+      Maskstore.limit(1000).each do |store|
+        @places << [
+          store.id,
+          store.code,
+          store.name,
+          store.address,
+          store.latitude,
+          store.longitude,
+          status_sample
+        ]
+      end
+    elsif @category == "선별진료소"
+    elsif @category == "shelters"
+      @shelters = Shelter.limit(50)
+      @shelters.each do |shelter|
+        @places << [
+          shelter.id,
+          shelter.status,
+          shelter.name,
+          shelter.road_address,
+          shelter.y.to_f,
+          shelter.x.to_f,
+          status_sample
+        ]
+      end
     end
+  end
+
+  def status_sample
+    ["red", "orange", "green", "black"].sample
   end
 end
