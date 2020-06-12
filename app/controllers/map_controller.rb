5d285fe5fc29f7608e89e16d8a9846f22c11576b
rest515 <rest515@gmail.com>
2020-06-12 20:45:32 +0900

    대피소 데이터를 지도위에 보여줍니다

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,4 +1,17 @@
 class MapController < ApplicationController
   def index
+    @places = []
+
+    @shelters = Shelter.limit(50)
+    @shelters.each do |shelter|
+      @places << [
+        shelter.id,
+        shelter.status,
+        shelter.name,
+        shelter.road_address,
+        shelter.y.to_f,
+        shelter.x.to_f
+      ]
+    end
   end
 end
--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,2 +1,9 @@
 class Shelter < ApplicationRecord
+  def x_num
+    x.to_f
+  end
+
+  def y_num
+    y.to_f
+  end
 end
